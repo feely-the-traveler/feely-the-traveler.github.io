ccb3dbb218eef7aacfd4d978cf7ac6342a525729
Jae Shin Yoon <jaeyoon@adobe.com>
2026-08-05 10:24:08 -0700

    Add ep03 to chapter 1 and rebuild gallery data

--- assets/js/gallery-data.js
+++ assets/js/gallery-data.js
@@ -27,7 +27,8 @@ const GALLERY = [
     title: "Everyday Life",
     works: [
       { file: "chapter/ch1/ep01.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...When life feels heavy, I look at the sky. \nMaybe it's the quiet. Maybe it's the endless blue.\nFor a moment, I believe everything will be okay.\nTomorrow still worries me. But today, the sky is enough....\"" },
-      { file: "chapter/ch1/ep02.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...Sometimes I feel low.\nMaybe it's the rain. Maybe it's comparison. Maybe I'm just tired.\nStrangely, those days help me see myself more honestly...\"" }
+      { file: "chapter/ch1/ep02.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...Sometimes I feel low.\nMaybe it's the rain. Maybe it's comparison. Maybe I'm just tired.\nStrangely, those days help me see myself more honestly...\"" },
+      { file: "chapter/ch1/ep03.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...I didn't know love could feel this quiet.\nIt isn't loud. It simply stays.\nLike roots beneath a tree, always there, even when you can't see them....\"" }
     ]
   }
 ];

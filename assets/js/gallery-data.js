f95d22b83ae9324db41e0cf1052fc5c12f14264d
Jae Shin Yoon <jaeyoon@adobe.com>
2026-08-11 10:01:28 -0700

    Add ep08 to chapter 1 and rebuild gallery data

--- assets/js/gallery-data.js
+++ assets/js/gallery-data.js
@@ -32,7 +32,8 @@ const GALLERY = [
       { file: "chapter/ch1/ep04.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...Some people don't have to do anything.\nJust being there is enough to brighten my day.....\"" },
       { file: "chapter/ch1/ep05.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...Some mornings don't need a plan. \nJust a warm cup.\nSlow conversations.\nAnd nowhere else to be....\"" },
       { file: "chapter/ch1/ep06.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...I don't know if any of this will work.\nI just know I'll try again tomorrow...\"" },
-      { file: "chapter/ch1/ep07.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...When my thoughts become too heavy, I go to sleep.\nNot to escape them. Just hoping tomorrow will carry a lighter heart...\"" }
+      { file: "chapter/ch1/ep07.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...When my thoughts become too heavy, I go to sleep.\nNot to escape them. Just hoping tomorrow will carry a lighter heart...\"" },
+      { file: "chapter/ch1/ep08.jpg", tag: "", emotion: "", primary: "", support: ["", ""], note: "\"...I miss my father sometimes.\nWhen I do, I miss home too.\nAnd somehow, I start missing everything I can't go back to...\"" }
     ]
   }
 ];
